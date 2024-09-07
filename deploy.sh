12af8e4ccaa3d5a309b546d57b04924f6a035143
lonelam <z@laizn.com>
2024-09-08 05:28:03 +0800

    feat: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rsync -av --exclude='*.js.map' dist docker-compose.yml package.json pnpm-lock.yaml root@laizn.com:~/aiweb/
 
-# remote: pm2 start dist/src/main.js
+# remote: pm2 start ~/aiweb/dist/src/main.js
